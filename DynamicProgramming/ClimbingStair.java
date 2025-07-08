2ab87457c1056d581f5b09cf4081b9eb1d1ab603
Nikhil <nikhilgupta2533@gmail.com>
2025-07-08 12:49:35 +0530

    Climbing Stair Problem

---
+++ DynamicProgramming/ClimbingStair.java
@@ -0,0 +1,13 @@
+package DynamicProgramming;
+
+public class ClimbingStair {
+    public static int climb(int n) {
+        if (n == 0 || n == 1) return 1;
+        return climb(n - 1) + climb(n - 2);
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(climb(n));
+    }
+}
