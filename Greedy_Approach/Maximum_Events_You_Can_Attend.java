22c1ae9e3565936dd8ae37c3f81824fa912c7023
Nikhil <nikhilgupta2533@gmail.com>
2025-07-09 09:25:26 +0530

    Issues Resolved

--- Greedy_Approach/Maximum_Events_You_Can_Attend.java
+++ Greedy_Approach/Maximum_Events_You_Can_Attend.java
@@ -11,33 +11,25 @@ public class Maximum_Events_You_Can_Attend {
             events[i][0] = sc.nextInt();
             events[i][1] = sc.nextInt();
         }
-
         Arrays.sort(events, Comparator.comparing(a -> a[0]));
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
         int day = 1;
         int i = 0;
         int res = 0;
         while (i < n || !minHeap.isEmpty()) {
-
             while (i < n && events[i][0] == day) {
                 minHeap.offer(events[i][1]);
                 i++;
             }
-
-
             while (!minHeap.isEmpty() && minHeap.peek() < day) {
                 minHeap.poll();
             }
-
-
             if (!minHeap.isEmpty()) {
                 minHeap.poll();
                 res++;
             }
-
             day++;
         }
-
         System.out.println(res);
     }
 }
